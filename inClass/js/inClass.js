001902540dc377bc862e7056bbed514df313c860
ThorneBush <morganthethornbush@gmail.com>
2022-10-11 17:04:41 -0700

    Lab 10 ubdate

--- inClass/js/inClass.js
+++ inClass/js/inClass.js
@@ -1,15 +1,29 @@
 
-document.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
     // ========= BUTTON =========//
 
-    let button = document.querySelector("#btn");
-
+    let btn = document.querySelector("#btn");
+    
     let handler = (e) => {
         console.log('you clicked');
         console.log(e);
     };
 
-    button.addEventListener("click", handler);
+    btn.addEventListener("click", handler);
+
+
+
+// ========= MULTIPLE BUTTONS =========//
+
+    let btn1 = document.querySelector('#btn1');
+    let btn2 = document.querySelector('#btn2');
+
+    let changeText = (e) => {
+        e.currentTarget.textContent = 'You Clicked Me!'
+    };
+
+  btn1.addEventListener('click', changeText);
+  btn2.addEventListener('click', changeText);
 
 });
--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -5,10 +5,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 // update the textcontent
-let courseName = document.querySelector('#courseName');
-let courseDescription = document.querySelector('#courseDescription');
-courseName.textContent = "CST 252"
-courseDescription.textContent = "This is a scripting class"
+
 
 // update the picture
 
@@ -18,6 +15,9 @@ courseDescription.textContent = "This is a scripting class"
 // mouseout: border reverts back to original state
 
     let button = document.querySelector(".itembox");
+
+
+
 
     let highLightBox = (e) => {
         button.style.border = 'thick solid grey';
@@ -29,23 +29,46 @@ courseDescription.textContent = "This is a scripting class"
     button.addEventListener("mouseover", highLightBox);
     button.addEventListener("mouseout", off);
 
+
+
+
+
+
+
 //Select the course box
 // Click: info pannel opens
-let infopanel = document.querySelector("#infopanel");
+
+
+
 
 let showPannel = (e) => {
-    console.log(e);
+
+    let infopanel = document.querySelector("#infopanel");
     infopanel.style.display = 'block';
+
+    let courseName = document.querySelector('#courseName');
+    let courseDescription = document.querySelector('#courseDescription');
+
+    courseName.textContent = "CST 252"
+    courseDescription.textContent = "This is a scripting class"
   }
 
 
 button.addEventListener("click", showPannel);
 
+
+
+
+
+
+
+
 //Select the exit button
 // Click: closes info pannel
 let exitButton = document.querySelector("#exitbutton");
+
 let closePannel = (e) => {
-    console.log(e);
+  
     infopanel.style.display = 'none';
   }
 
